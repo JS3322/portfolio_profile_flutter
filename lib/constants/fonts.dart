981493e49c586e33c16c814c05c695c3010de98e
“js” <bada@ganada.kr>
2021-01-24 15:37:00 +0900

    20210124_fix_font1

--- lib/constants/fonts.dart
+++ lib/constants/fonts.dart
@@ -5,4 +5,7 @@ class Fonts {
   static const String nexa_light = 'NexaLight';
   static const String nexa_bold = 'NexaBold';
 
+  static const String test1 = 'NexaBold';
+  static const String test2 = 'NexaBold';
+  static const String test3 = 'NexaBold';
 }
