f52257e4cbd50af4f769a5df0768ac8821f36b82
“js” <bada@ganada.kr>
2021-01-24 15:20:14 +0900

    20210124_fix_string_1

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -9,12 +9,11 @@ class Strings {
   static const String headline = '안녕하세요. 생각하고 이야기를 나누는 것을 좋아하는 J입니다.';
   static const String summary = '눈이 오는 날은 겨울입니다. 춥네요.';
   static const String experience = '참여 프로젝트';
-  static const String skills_i_have = '나의 기술 스택';
-  static const String rights_reserved =
-      '© 2021 GANADA STEP. ALL RIGHTS RESERVED.';
+  static const String skills_i_have = '주요 기술';
+  static const String rights_reserved = 'P. 2021 GANADA STEP. WELCOME.';
 
   // menu items
   static const String menu_home = 'Home';
   static const String menu_about = 'History';
-  static const String menu_contact = 'Mini Project';
+  static const String menu_contact = 'Mini Project(정리중)';
 }
