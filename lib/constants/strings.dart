3a4574e42178cbd6c5f32c86a53a0b69311b13a2
“js” <bada@ganada.kr>
2021-01-28 12:12:57 +0900

    20210128_string_fix1

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -4,8 +4,8 @@ class Strings {
   //intro text
   static const String about = '나';
   static const String me = '의 소개';
-  static const String portfoli = '포트폴리';
-  static const String o = '오';
+  static const String portfoli = 'Our Object Undi';
+  static const String o = 'fined';
   static const String headline = '안녕하세요. 생각하고 이야기를 나누는 것을 좋아하는 J입니다.';
   static const String summary = '눈이 오는 날은 겨울입니다. 춥네요.';
   static const String experience = '참여 프로젝트';
